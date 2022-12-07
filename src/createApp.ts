52648f5cbcf8e03fc4ace751dce75d36128178c8
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-12-07 16:57:31 +0000

    smile

--- src/createApp.ts
+++
@@ -1,6 +0,0 @@
-import express, { Express } from 'express'
-import expressUtils, { ConfigOptions } from './expressUtils'
-
-export default (arg?: ConfigOptions): Express => {
-  return expressUtils(express(), arg)
-}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,53 @@
-import createApp from './createApp.js'
-import expressUtils from './expressUtils.js'
+import express, { Express, Request, Response } from 'express'
 
-export { expressUtils }
-export default createApp
+export type ConfigOptions = {
+  ping?: string
+}
+
+type FieldsType = (string | string[])[]
+declare global {
+  namespace Express {
+    export interface Response {
+      success: Function
+    }
+    export interface Request {
+      getBody(this: Request, ...fields: FieldsType): any
+    }
+  }
+}
+
+const successMethod = function (this: Response, data: any, code = 200) {
+  const resData = { status: 'success', data }
+  this.status(code).json(code === 204 ? null : resData)
+  return resData
+}
+
+const getBodyMethod = function (this: Request, ...fields: FieldsType) {
+  const newObj: any = {}
+  const fieldsFlat: string[] = fields
+    .flat()
+    .map((field) => field.split(' '))
+    .flat()
+    .filter((field) => field.trim())
+
+  for (let field of fieldsFlat) {
+    const value = this.body[field]
+    if (value !== undefined) newObj[field] = value
+  }
+  return newObj
+}
+
+const pingController = (req: Request, res: Response) => {
+  res.status(200).end()
+}
+
+export default ({ ping = '' }: ConfigOptions = {}): Express => {
+  const app = express()
+
+  app.response.success = successMethod
+  app.request.getBody = getBodyMethod
+
+  if (ping) app.use(ping, pingController)
+
+  return app
+}
