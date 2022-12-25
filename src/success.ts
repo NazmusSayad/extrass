c5cb0e4489f0475b13b43f2ebd1fc34c6d4c0374
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2022-12-25 09:57:41 +0000

    M package.json  M src/success.ts

--- src/success.ts
+++ src/success.ts
@@ -1,17 +1,12 @@
 import { Response } from 'express'
 
-const successMethod: SussessMethod = function (
-  this: Response,
-  data: any,
-  code = 200
-) {
+const successMethod: SussessMethod = function (this: Response, data) {
   const resData = { status: 'success', data }
-  this.status(code).json(code === 204 ? null : resData)
+  this.json(resData)
   return resData
 }
 
 export default successMethod
 export type SussessMethod = <Data extends unknown>(
-  data: Data,
-  code?: number
+  data: Data
 ) => { status: string; data: Data }
