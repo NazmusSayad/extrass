2c7b792ca1dfbf7cb93c3706edd536b04a23d541
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-09-14 17:47:59 +0600

    rype error handling

--- src/rypeHandlers.ts
+++ src/rypeHandlers.ts
@@ -2,7 +2,7 @@ import { GetErrorInfoHandler } from 'req-error'
 
 const rypeHandlers: GetErrorInfoHandler[] = [
   function (err) {
-    if (err.isRypeClientError) {
+    if (err.isRypeError && err.isRypeClientError) {
       return [err.message, 400]
     }
   },
